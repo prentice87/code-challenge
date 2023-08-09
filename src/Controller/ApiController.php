ca1415c1bbc143470297ef394b10c751a7c1bcb5
Thomas Prentice <tprentice@mobile-office.de>
2023-08-09 11:34:42 +0200

    twig added

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,11 +3,45 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class ApiController
-{
-    private $apiClient;
+use App\WizardWorld\Service\ApiClient;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
-    public function __construct()
+class ApiController extends AbstractController
+{
+    public function __construct(
+        private ApiClient $apiClient
+    ){}
+
+    public function index(Request $request): Response
     {
+        return $this->render('index.html.twig',[]);
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function houses(Request $request): Response
+    {
+        $houses = $this->apiClient->fetchHouses();
+
+        $data = print_r($houses, true);
+
+        return $this->render('houses.html.twig',['data' => $data]);
+    }
+
+    public function house(Request $request): Response
+    {
+
     }
 }
