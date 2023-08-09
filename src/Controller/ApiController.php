1f2c5f6234218e336c4ba29f41f8e8122447241b
Thomas Prentice <tprentice@mobile-office.de>
2023-08-09 13:28:24 +0200

    finished index and houses

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\WizardWorld\Common\UuidCollection;
+use App\WizardWorld\Common\ValueObject\Uuid;
 use App\WizardWorld\Service\ApiClient;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,9 +22,20 @@ class ApiController extends AbstractController
         private ApiClient $apiClient
     ){}
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws Exception
+     */
     public function index(Request $request): Response
     {
-        return $this->render('index.html.twig',[]);
+        $houses = $this->apiClient->fetchHouses();
+        $traitCollection = $houses->getAllTraitsCollection();
+
+        return $this->render('index.html.twig',['traits' => $traitCollection->all()]);
     }
 
     /**
@@ -30,18 +44,28 @@ class ApiController extends AbstractController
      * @throws RedirectionExceptionInterface
      * @throws DecodingExceptionInterface
      * @throws ClientExceptionInterface
+     * @throws Exception
      */
     public function houses(Request $request): Response
     {
+        $traits = $request->request->all('traits');
+        $selectedTraitsUuidCollection = UuidCollection::createFromRequestPostArray($traits);
         $houses = $this->apiClient->fetchHouses();
+        $filteredHouses = $houses->getFilteredByTraitsUuidCollection($selectedTraitsUuidCollection);
 
-        $data = print_r($houses, true);
-
-        return $this->render('houses.html.twig',['data' => $data]);
+        return $this->render('houses.html.twig',['houses' => $filteredHouses, 'selectedTraits' => $selectedTraitsUuidCollection]);
     }
 
+    /**
+     * @throws Exception
+     */
     public function house(Request $request): Response
     {
+        $id = $request->attributes->get('id');
+        $uuid = Uuid::createFromString($id);
 
+        $house = $this->apiClient->fetchHouse($uuid);
+
+        return $this->render('house.html.twig',[]);
     }
 }
